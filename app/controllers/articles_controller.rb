5c92d5e2be629c80aa74f8db5421eb6724f95383
Tuğba Özaydın <tbzndtbzn206@gmail.com>
2017-08-19 10:38:09 +0300

    Welcome,Article and Comment controller created

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    get_article
+  end
+
+  def edit
+    get_article
+  end
+
+  def update
+    get_article
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    get_article
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def get_article
+    @article = Article.find(params[:id])
+  end
+end
