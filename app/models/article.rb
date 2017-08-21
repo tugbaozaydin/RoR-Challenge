4e1db2a2a25b1c54f9b71c3aaf0ee3f83ea30db0
Tuğba Özaydın <tbzndtbzn206@gmail.com>
2017-08-21 11:19:59 +0300

    Category model and migration

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
 	 has_many :comments, dependent: :destroy
      validates :title, presence: true, length: { minimum: 5 }
+     many_to_many :categories
 end
---
+++ db/migrate/20170821081203_articles_categories.rb
@@ -0,0 +1,8 @@
+class ArticlesCategories < ActiveRecord::Migration[5.1]
+  def change
+  	create_join_table :articles, :categories do |t|
+      t.index :article_id
+      t.index :category_id
+  end
+  end
+end
