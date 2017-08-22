9752a3726cfe50adb72c8a7e8bb814ef37f3ba50
Tuğba Özaydın <tbzndtbzn206@gmail.com>
2017-08-22 14:17:39 +0300

    articles_categories belongs_to changed

--- db/migrate/20170821081203_articles_categories.rb
+++ db/migrate/20170821081203_articles_categories.rb
@@ -1,8 +1,8 @@
 class ArticlesCategories < ActiveRecord::Migration[5.1]
   def change
   	create_join_table :articles, :categories do |t|
-      t.index :article_id
-      t.index :category_id
+      t.belongs_to :article_id
+      t.belongs_to :category_id
   end
   end
 end
